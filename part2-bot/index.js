4f5e56102d54d871f8bd82391d6e4b8b40e797a5
Deise Araújo <127044115+DehAraujo@users.noreply.github.com>
2025-11-10 03:19:00 +0000

    fix(server): Remove cabecalho Go (package main) do server.py.

--- part2-bot/index.js
+++ part2-bot/index.js
@@ -1,19 +1,19 @@
 const zmq = require("zeromq");
-
 const { randomUUID } = require("crypto");
 
-const user = "bot-" + randomUUID().slice(0, 4);
-const req = new zmq.Request();
-await req.connect("tcp://server:5555");
-
-const sub = new zmq.Subscriber();
-await sub.connect("tcp://server:5556");
-sub.subscribe(user);
-sub.subscribe("geral");
-
-console.log(`🤖 Bot ${user} iniciado`);
-
 (async () => {
+  const user = "bot-" + randomUUID().slice(0, 4);
+
+  const req = new zmq.Request();
+  await req.connect("tcp://server:5555");
+
+  const sub = new zmq.Subscriber();
+  await sub.connect("tcp://server:5556");
+  sub.subscribe(user);
+  sub.subscribe("geral");
+
+  console.log(`🤖 Bot ${user} iniciado`);
+
   while (true) {
     const msg = {
       service: "publish",
@@ -24,9 +24,11 @@ console.log(`🤖 Bot ${user} iniciado`);
         timestamp: Date.now(),
       },
     };
+
     await req.send(JSON.stringify(msg));
     const [reply] = await req.receive();
     console.log("📩 Resposta:", reply.toString());
+
     await new Promise(r => setTimeout(r, 5000));
   }
 })();
